feb50d89aeed6a668cf90c8903515d8dec2ef3ff
dasom1007 <nab1kong@naver.com>
2023-12-08 17:00:57 +0900

    자바 1일차 - 논리형 변수 선언

---
+++ java_ds/src/day01/VariableEx4.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class VariableEx4 {
+
+	//논리형 변수 선언 예제
+	public static void main(String[] args) {
+		
+		boolean isEven = true;
+		System.out.println(isEven);
+		
+		isEven = 2 % 2 == 0;
+		System.out.println("2 is Even number? " + isEven);
+		//다음주 학습 내용. 논리형 변수의 예제.
+		
+		
+	}
+
+}
