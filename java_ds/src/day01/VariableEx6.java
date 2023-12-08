7defeee1bfa813a989c32b7936177552cb3f5ef8
dasom1007 <nab1kong@naver.com>
2023-12-08 17:01:17 +0900

    자바 1일차 - 변수명 작성 규칙

---
+++ java_ds/src/day01/VariableEx6.java
@@ -0,0 +1,36 @@
+package day01;
+
+public class VariableEx6 {
+
+	// 변수명 작성 규칙(필수)과 관례(선택이지만 추천)
+	public static void main(String[] args) {
+		
+		int num;
+		int NUM;
+		int Num;
+		int nuM;
+		//1. 대소문자 구분
+		
+		//int int;
+		//int public;
+		//int class;
+		//2. 예약어는 사용 불가능
+		
+		int num2;
+		//int 2num;
+		//3. 숫자로 시작 불가능
+		
+		int _num;
+		//int #num;
+		//int n um;
+		int num$;
+		//4. 특수문자는 _와 $만 가능
+		
+		//int num;
+		//5. 중복 선언 불가능. 위에서 선언돼서 불가.
+		
+		
+		
+	}
+
+}
