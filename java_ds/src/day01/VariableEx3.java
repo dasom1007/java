765147b625143903e898eae73cf7bb2798190993
dasom1007 <nab1kong@naver.com>
2023-12-08 17:00:49 +0900

    자바 1일차 - 실수형 변수 선언

---
+++ java_ds/src/day01/VariableEx3.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class VariableEx3 {
+
+	//실수형 변수 선언 예제 //alt+클릭 줄바꿈 가능
+	public static void main(String[] args) {
+		
+		float num1 = 1.23f; //실수 리터럴 1.23의 타입은 double
+		//1.23을 float에 저장하려면 접미사 f를 붙여야 한다.
+		System.out.println(num1);
+		
+		double num2 = 3.14;
+		System.out.println(num2);
+		
+
+	}
+
+}
