7e1f5f6f832dd04b63e8e96b296616726aa7da67
dasom1007 <nab1kong@naver.com>
2023-12-12 17:02:52 +0900

    자바 3일차 For문 예제 - a부터z까지 반복문 사용해서 출력

---
+++ java_ds/src/day03/AlphabetEx1.java
@@ -0,0 +1,20 @@
+package day03;
+
+public class AlphabetEx1 {
+
+	public static void main(String[] args) {
+		/*
+		 * a부터 z까지 반복문을 이용하여 출력하는 코드를 작성.
+		 * 반복회수   : A~Z까지
+		 * 규칙성    : ch 출력
+		 */
+		
+
+		for( char ch = 'a'; ch <= 'z'  ; ch++ ) {
+			System.out.print(ch);
+		    	
+		}
+
+	}
+
+}
