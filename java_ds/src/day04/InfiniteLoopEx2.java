d7eb0c518a58bf5d5bfa3f07f872d7a4ebf7d041
dasom1007 <nab1kong@naver.com>
2023-12-13 17:25:56 +0900

    자바 4일차 - 무한 루프 예제2

---
+++ java_ds/src/day04/InfiniteLoopEx2.java
@@ -0,0 +1,19 @@
+package day04;
+
+import java.util.Scanner;
+
+public class InfiniteLoopEx2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num;
+		for(int i = 10; i>0 ; ) {
+		   	System.out.println("정수 입력 :");
+			num = scan.nextInt();
+			System.out.println("정수 출력 :" + num);
+	}
+			System.out.println("프로그램 종료");
+}
+}
