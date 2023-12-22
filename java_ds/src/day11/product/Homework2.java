33aa8ee6eb603d6c68483eaef8988593cace8b5d
dasom1007 <nab1kong@naver.com>
2023-12-22 17:45:49 +0900

    자바 11일차 - 숙제2

--- java_ds/src/day11/product/Homework2.java
+++ java_ds/src/day11/product/Homework2.java
@@ -1,10 +1,57 @@
 package day11.product;
 
+import java.util.Scanner;
+
 public class Homework2 {
+	private static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* 두 정수와 산술 연산자를 입력받아 결과를 출력하는 코드를 작성
+		 * 메서드를 생성
+		 */
+		
+		//반복문
+		
+		//정수를 입력
+		System.out.print("숫자(정수)를 입력 : ");
+		int num1 = scan.nextInt();
+		
+		System.out.print("연산자(+, -, %, /)를 입력 : ");
+		int op = scan.nextInt();
+		
+		System.out.print("숫자(정수)를 입력 : ");
+		int num2 = scan.nextInt();
+
+		int answer = 0;
+		switch(op) {
+		
+		case '+':
+				answer = num1 + num2;
+			break;
+			
+		case '-':
+				answer = num1 - num2;
+			break;
+			
+		case '*':
+				answer = num1 * num2;
+			break;
+			
+		case '/':
+				answer = num1 / num2;
+			break;
+		
+		}
+		
+		System.out.println(num1 + op + num2 + "=" + answer );
+			
+		}
 
 	}
+	
+	
+	
+	
+	
+	
 
-}
--- java_ds/src/day11/product/HomeworkEx3.java
+++ java_ds/src/day11/product/HomeworkEx3.java
@@ -2,9 +2,82 @@ package day11.product;
 
 public class HomeworkEx3 {
 
+	/* 사이트에서 회원 관리(로그인, 회원가입 등)를 위한 Member 클래스를 작성 */
+	
+	//작성한 Member 클래스를 이용하여 인스턴스를 생성하고 구현한 메서드를 테스트
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
+
+//필요한 멤버변수 및 메서드를 정리하고 구현
+class Member{
+	String name, id, password, login, join;
+	//회원이름, 아이디, 비밀번호, 로그인, 회원가입
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getJoin() {
+		return join;
+	}
+
+	public void setJoin(String join) {
+		this.join = join;
+	}
+	
+	/**
+	 * 생성자
+	 * @param name
+	 * @param id
+	 * @param password
+	 * @param login
+	 * @param join
+	 */
+	public Member(String name, String id, String password, 
+			String login, String join) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		this.login = login;
+		this.join = join;
+	}
+	
+	public void loginprint() {
+		System.out.println("아이디 : ");
+	}
+}
+
+
+
+
+
